6a52a2e743a1d793b6e5fd9ac7225bf70380da34
Kevin Johnson <johnsonk_nyc@yahoo.com>
2023-05-11 19:10:57 +0200

    Portfolio

---
+++ src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import {
+	EmailShareButton,
+	FacebookShareButton,
+	LinkedinShareButton,
+	TwitterShareButton,
+	WhatsappShareButton,
+} from "react-share";
+
+import { EmailIcon, FacebookIcon, LinkedinIcon, TwitterIcon, WhatsappIcon } from "react-share";
+
+const Footer = () => {
+	const shareUrl = "localhost:3000"; // TODO: change to actual url
+
+	return (
+		<div className={styles.footer}>
+			<div className={styles.container}>
+				<div className={styles.left}>
+					<div className={styles.share}>
+						<TwitterShareButton url={shareUrl} title="title" className={styles.shareButton}>
+							<TwitterIcon size={32} round={true} />
+						</TwitterShareButton>
+						<FacebookShareButton url={shareUrl} title="title" className={styles.shareButton}>
+							<FacebookIcon size={32} round={true} />
+						</FacebookShareButton>
+						<LinkedinShareButton url={shareUrl} title="title" className={styles.shareButton}>
+							<LinkedinIcon size={32} round={true} />
+						</LinkedinShareButton>
+						<WhatsappShareButton url={shareUrl} title="title" className={styles.shareButton}>
+							<WhatsappIcon size={32} round={true} />
+						</WhatsappShareButton>
+						<EmailShareButton url={shareUrl} title="title" className={styles.shareButton}>
+							<EmailIcon size={32} round={true} />
+						</EmailShareButton>
+					</div>
+				</div>
+				<div className={styles.right}>
+					<ul>
+						<li>
+							<a href="#">Home</a>
+						</li>
+						<li>
+							<a href="#">About</a>
+						</li>
+						<li>
+							<a href="#">Projects</a>
+						</li>
+						<li>
+							<a href="#">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<p>Copyright Kevin Johnson - {new Date().getFullYear()}</p>
+		</div>
+	);
+};
+
+export default Footer;
---
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { BsCodeSlash } from "react-icons/bs";
+
+const Navbar = () => {
+	return (
+		<div className={styles.navbar}>
+			<div className={styles.container}>
+				<div className={styles.left}>
+					<a href="#">
+						<BsCodeSlash />
+					</a>
+				</div>
+				<div className={styles.right}>
+					<ul>
+						<li>
+							<a href="#">Home</a>
+						</li>
+						<li>
+							<a href="#">About</a>
+						</li>
+						<li>
+							<a href="#">Projects</a>
+						</li>
+						<li>
+							<a href="#">Contact</a>
+						</li>
+					</ul>
+					<div className={styles.socials}>
+						<a href="#">
+							<AiFillGithub />
+						</a>
+						<a href="#">
+							<AiFillLinkedin />
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
