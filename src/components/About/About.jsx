94dbed588f73e118580d9af118e23481e7e5bdf8
Kevin Johnson <johnsonk_nyc@yahoo.com>
2023-05-11 20:01:22 +0200

    hero page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import styles from "./About.module.css";
 
 const About = () => {
-	return <div>About</div>;
+	return (
+		<div>
+			<p>
+				Hello, my name is Kevin. I am a junior front end developer. I am currently studying at Hyper Island in
+				Stockholm, Sweden. I am a former psychology counselor with a passion for learning and developing my skills with
+				web development. I have started my journey in web development in 2022. I enjoy learning and being a part of a
+				team. I am currently the student representative in my class for both semesters and played a role in shaping the
+				curriculum for the next group of rising talents. I am looking forward to sharping my skills and bringing the
+				best not only for myself but also for others around me.
+			</p>
+		</div>
+	);
 };
 
 export default About;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import styles from "./Hero.module.css";
 
 const Hero = () => {
-	return <div className={styles.container}>Hero</div>;
+	return (
+		<div className={styles.hero}>
+			<div className={styles.container}>
+				<div className={styles.wrapper}>
+					<div className={styles.image_wrapper}>
+						<img src="/images/head_shot.jpg" alt="hero" />
+					</div>
+					<h1>Hi, I'm Kevin Johnson</h1>
+					<p>
+						<b>Junior Frontend Developer</b> based in Stockholm. <br />
+						<b>Currently</b> looking to land that internship...
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
